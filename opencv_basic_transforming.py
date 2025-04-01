5f397b25588e9ee9f0b78bd7eabf613c612f3213
Kacper Mitręga <mitrega.kacper04@gmail.com>
2025-04-01 14:08:36 +0200

    ?

--- opencv_basic_transforming.py
+++ opencv_basic_transforming.py
@@ -1,9 +1,27 @@
-# import the necessary packages
+
 import numpy as np
 import cv2
-# load the image and display it to our screen
+import imutils
+
 image = cv2.imread("example.jpg")
 cv2.imshow("Original", image)
-# shift the image 25 pixels to the right and 50 pixels down
-M = np.float32([[1, 0, 25], [0, 1, 50]])
-shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+
+M = np.float32([[1, 0, 30], [0, 1, 40]])
+shifted1 = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow("Shifted1", shifted1)
+
+M = np.float32([[1, 0, -20], [0, 1, -50]])
+shifted2 = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow("shifted2", shifted2)
+cv2.waitKey(0)
+
+
+shifted3 = imutils.translate(image, 100, 50)
+cv2.imshow("Shifted3", shifted3)
+cv2.waitKey(0)
+
+X = int(input("Podaj przesunięcie X: "))
+Y = int(input("Podaj przesunięcie Y: "))
+shifted4 = imutils.translate(image, X, Y)
+cv2.imshow("Shifted4", shifted4)
+cv2.waitKey(0)
